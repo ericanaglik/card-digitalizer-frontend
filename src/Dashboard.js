544fd450b294f0eece46c842585cea1ec7df15b7
erica2605@live.com <erica2605@live.com>
2020-03-24 13:40:28 -0700

    added text to dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -31,12 +31,12 @@ class Dashboard extends Component {
 										<img src={data.img_url}/>
                     <div>
                     <h2 className="title">{data.name}</h2>
-													<li className="cardinfo">{data.stage}</li>
-													<li className="cardinfo">{data.type_1}</li>
-                          <li className="cardinfo">{data.type_2}</li>
-													<li className="cardinfo">{data.series}</li>
-                          <li className="cardinfo">{data.rarity}</li>
-													<li className="cardinfo">{data.year_released}</li>
+													<li className="cardinfo">Stage: {data.stage}</li>
+													<li className="cardinfo">Type 1: {data.type_1}</li>
+                          <li className="cardinfo">Type 2: {data.type_2}</li>
+													<li className="cardinfo">Series: {data.series}</li>
+                          <li className="cardinfo">Rarity: {data.rarity}</li>
+													<li className="cardinfo">Year Released: {data.year_released}</li>
 												</div>
                         </div>
 								</div>
